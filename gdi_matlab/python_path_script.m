b00b7bfa10c2e17a6bfb9e0fb71ab1d558680522
jy46 <45184853+jy46@users.noreply.github.com>
2020-10-09 18:10:22 -0500

    Update python_path_script.m

--- gdi_matlab/python_path_script.m
+++ gdi_matlab/python_path_script.m
@@ -1,3 +1,4 @@
 % This script will be called by ccdi_mat_matlab_fun() when using python for estimating GDI
-% You should put your python path in between the ''
+% You should put your system path and python path in between the '' for each
+system_path = '';
 python_path = '';
